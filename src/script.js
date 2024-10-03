96bd280575182fc86c7f8e5adb68651ed7a62df1
ivanreeve <ivanreevelopez@gmail.com>
2024-10-03 21:29:59 +0800

    fix gitignore

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
-var e = document.querySelector(".output #output-value");
-e.textContent = "aas";
+function history() {
+    return document.getElementById("history-value").innerText;
+}
+
+function outputhistory(num) {
+    document.getElementById("history-value").innerText=num;
+}
+
+function output() {
+    return document.getElementById("output-value").innerText;
+}
+
+function outputshow(num) {
+    if(num=="") {
+        document.getElementById("output-value").innerText=num;
+    } else {
+        document.getElementById("output-value").innerText=getFormattedNumber(num);
+    }         
+} 
